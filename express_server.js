6049c18497cc26746db1d259f01e13303be5850f
Kenny Moon <kennymoon90@gmail.com>
2022-06-13 18:40:06 -0700

    added body-parser npm package, created url submit form

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +12,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  let result = '';
+  for (let i = 0; i < 7 i++) {
+    result += characters.charAt(Math.floor(Math.random() * 62));
+  }
+  return result;
+};
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
@@ -18,6 +34,11 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (req, res) => {
