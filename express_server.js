f4d4ed030eaca1a5a4e013df1a1e58b900a7ac39
Kenny Moon <kennymoon90@gmail.com>
2022-06-14 19:25:07 -0700

    created a username login in the header and a post to track the username cookie

--- express_server.js
+++ express_server.js
@@ -57,6 +57,12 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
 app.post("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
