dbd8a8b0d0f80eaff1ef683f1d65b29923a7439c
Kenny Moon <kennymoon90@gmail.com>
2022-06-17 00:09:36 -0700

    moved all helper functions to helpers.js and exported them

---
+++ helpers.js
@@ -0,0 +1,34 @@
+const checkUserEmails = function(email, database) {
+  for (let user in database) {
+    if (database[user]['email'] === email) {
+      return database[user];
+    }
+  }
+  return null;
+};
+
+const findURLsById = function(userId, database) {
+  let urlDatabaseById = {};
+  for (let url in database) {
+    if (database[url].userID === userId) {
+      urlDatabaseById[url] = database[url];
+    }
+  }
+  return urlDatabaseById;
+};
+
+const generateRandomString = function() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  let result = '';
+  for (let i = 0; i < 7; i++) {
+    result += characters.charAt(Math.floor(Math.random() * 62));
+  }
+  return result;
+};
+
+module.exports = {
+  checkUserEmails,
+  findURLsById,
+  generateRandomString
+};
